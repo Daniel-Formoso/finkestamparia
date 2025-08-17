7a796da679bccd9b138cd04da863a057f75b6f8a
Daniel Formoso <118393625+Daniel-Formoso@users.noreply.github.com>
2025-08-17 04:34:10 -0300

    modal bg

--- src/app/components/Modal/modal.tsx
+++ src/app/components/Modal/modal.tsx
@@ -20,20 +20,21 @@ const Modal = forwardRef<ModalHandle, ModalProps>((props, ref) => {
   }));
 
   return (
-    <dialog ref={dialogRef} className="modal bg-transparent backdrop:bg-black/50 backdrop:backdrop-blur-sm">
-      <div className="modal-box border-1 border-[#EFA531]">
-        <h3 className="font-bold text-[#EFA531] text-lg">{props.title}</h3>
-        <div className="py-4 text-white font-semibold">{props.children}</div>
-        <div className="modal-action">
-          <button
-            type="button"
-            className="btn btn-outline bg-[#EFA531] text-white border-[#EFA531] hover:bg-white hover:text-[#EFA531] px-10"
-            onClick={() => dialogRef.current?.close()}
-          >
-            Fechar
-          </button>
-        </div>
+    <dialog ref={dialogRef} className="modal">
+    <div className="modal-box border border-[#EFA531] bg-[#1D232A] rounded-lg p-6">
+      <h3 className="font-bold text-[#EFA531] text-lg">{props.title}</h3>
+      <div className="py-4 text-white font-semibold">{props.children}</div>
+      <div className="modal-action">
+        <button
+          type="button"
+          className="btn btn-outline bg-[#EFA531] text-white border-[#EFA531] hover:bg-white hover:text-[#EFA531] px-10"
+          onClick={() => dialogRef.current?.close()}
+        >
+          Fechar
+        </button>
       </div>
+    </div>
+
     </dialog>
   );
 });
